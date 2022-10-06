e52f4cd1ff7f3f593dfc72687af144c0304dd9a9
mridul21harish <62094433+mridul21harish@users.noreply.github.com>
2022-10-06 15:07:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from graph import Graph
+
+# Main driver function for the NetList Viewer and Simulator
+if __name__ == "__main__":
+    # Initialize the graph
+    graph = Graph()
+    # Construct the graph
+    graph.construct()
+    # Simulate the graph
+    graph.simulate()
+    # Simulate the TMR approach
+    graph.TMRApproach()
